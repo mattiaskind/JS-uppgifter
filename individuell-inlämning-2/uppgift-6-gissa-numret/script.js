bb44ad8fbd370b18ac27febb8617dfcf62a04293
Mattias Kind <mattias.kind@gmail.com>
2021-11-19 09:24:53 +0100

    Update assignment 6

--- individuell-inlämning-2/uppgift-6-gissa-numret/script.js
+++ individuell-inlämning-2/uppgift-6-gissa-numret/script.js
@@ -1,60 +1,102 @@
 'use strict';
 
-let gameState = {
+const NUMBER_OF_GUESSES = 5;
+
+const gameState = {
   guess: null,
-  guessesLeft: 5,
+  guessesLeft: NUMBER_OF_GUESSES,
   answer: Math.floor(Math.random() * 100 + 1),
+  play: true,
 };
 
+const responses = {
+  low: ['För lågt', 'Gissa högre', 'Tyvärr, det är högre'],
+  high: ['För högt', 'Gissa lägre', 'Tyvärr, lägre'],
+};
+
+// Paneler
 const panelTooLow = document.querySelector('.panel-guess-low');
 const panelTooHigh = document.querySelector('.panel-guess-high');
 const panelResult = document.querySelector('.panel-guess-result');
-const gameOverPanel = document.querySelector('.game-over');
-const btnPlayAgain = document.querySelector('.btn-play-again');
-const panelPlayAgainWin = document.querySelector('.play-again-win');
-const inputGuess = document.querySelector('#input-guess');
+const panelGameOver = document.querySelector('.panel-game-over');
+const panelWin = document.querySelector('.panel-win');
+
+// Knappar
 const btnGuess = document.querySelector('#btn-guess');
-const number = document.querySelector('.number');
+const btnPlayAgain = document.querySelectorAll('.btn-play-again');
+
+// Inputfält
+const inputGuess = document.querySelector('#input-guess');
+
+const number = document.querySelector('.number-of-guesses-left');
+const panelResultText = document.querySelector('.panel-guess-result--text');
 
 console.log('RÄTT SVAR ÄR: ' + gameState.answer);
 
-btnGuess.addEventListener('click', (e) => {
+number.innerHTML = NUMBER_OF_GUESSES;
+panelResultText.innerHTML = 'gissningar kvar';
+
+btnGuess.addEventListener('click', handleUserGuess);
+btnPlayAgain.forEach((btn) => {
+  btn.addEventListener('click', playAgain);
+});
+
+function playAgain() {
+  initGame();
+  if (!panelGameOver.classList.contains('hidden'))
+    panelGameOver.classList.add('hidden');
+  if (!panelWin.classList.contains('hidden')) panelWin.classList.add('hidden');
+}
+
+function handleUserGuess(e) {
   e.preventDefault();
-  if (!saveInput(inputGuess.value)) return;
-  if (gameState.guess > 100 || gameState.guess < 1)
+  if (!saveInput(inputGuess.value) || gameState.play === false) return;
+  if (gameState.guess > 100 || gameState.guess < 1) {
     console.log('För litet eller högt tal!');
+    return;
+  }
 
   gameState.guessesLeft--;
-  if (gameState.guessesLeft === 0) {
-    gameOver();
-  } else {
-    number.innerHTML = gameState.guessesLeft;
-  }
+  number.innerHTML = gameState.guessesLeft;
+
   checkGuess();
-});
+}
+
+function updatePanelStatus(currentPanel, newPanel) {
+  if (currentPanel.classList.contains('last-guess'))
+    currentPanel.classList.remove('last-guess');
+  newPanel.classList.add('last-guess');
+}
+
+function clearPanelStatus() {
+  console.log('CLEAR');
+}
 
 function checkGuess() {
   const { guess, answer } = gameState;
 
   if (guess > answer) {
-    panelTooLow.innerHTML = '';
-    if (panelTooLow.classList.contains('last-guess'))
-      panelTooLow.classList.remove('last-guess');
-    panelTooHigh.classList.add('last-guess');
-    panelTooHigh.innerHTML = 'För högt!';
+    clearPanels();
+    updatePanelStatus(panelTooLow, panelTooHigh);
+
+    const response = Math.floor(Math.random() * responses.high.length);
+    panelTooHigh.innerText = responses.high[response] + '!';
   }
   if (guess < answer) {
-    panelTooHigh.innerHTML = '';
-    if (panelTooHigh.classList.contains('last-guess'))
-      panelTooHigh.classList.remove('last-guess');
-    panelTooLow.classList.add('last-guess');
-    panelTooLow.innerHTML = 'För lågt!';
+    clearPanels();
+    updatePanelStatus(panelTooHigh, panelTooLow);
+
+    const response = Math.floor(Math.random() * responses.low.length);
+    panelTooLow.innerText = responses.low[response] + '!';
   }
   if (guess === answer) {
-    panelTooHigh.innerHTML = panelTooLow.innerHTML = '';
-    panelResult.innerHTML = answer;
-    number.innerHTML = 'RÄTT!';
-    panelPlayAgainWin.classList.remove('hidden');
+    clearPanels();
+    number.innerHTML = gameState.answer;
+    panelResultText.innerHTML = '';
+    panelWin.classList.remove('hidden');
+  } else if (gameState.guessesLeft === 0) {
+    gameState.play = false;
+    gameOver();
   }
 }
 
@@ -68,6 +110,23 @@ function saveInput(input) {
   return true;
 }
 
+function clearPanels() {
+  panelTooLow.innerHTML = panelTooHigh.innerHTML = '';
+}
+
 function gameOver() {
-  gameOverPanel.classList.remove('hidden');
+  panelGameOver.classList.remove('hidden');
+  initGame();
+}
+
+function initGame() {
+  gameState.guessesLeft = NUMBER_OF_GUESSES;
+  gameState.answer = Math.floor(Math.random() * 100 + 1);
+  gameState.play = true;
+
+  inputGuess.value = '';
+  number.innerHTML = NUMBER_OF_GUESSES;
+  panelResultText.innerHTML = 'gissningar kvar';
+
+  clearPanels();
 }
