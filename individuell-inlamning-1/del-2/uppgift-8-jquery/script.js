8c5a9712c8e1124522893d1dcf8c631fd73ece2d
Mattias Kind <mattias.kind@gmail.com>
2021-11-24 18:30:54 +0100

    Add new code for assignment 8

---
+++ individuell-inlamning-1/del-2/uppgift-8-jquery/script.js
@@ -0,0 +1,23 @@
+
+
+$('#fade-btn').click(function () {
+    $('#fade-content').fadeIn('slow');
+});
+
+
+$('#animate-container').click(function () {
+    $('#move').animate({
+        left: '500px'
+    });
+});
+
+$('#hover').hover(function () {
+    $('#hover').animate({
+        'border-radius': '50%'
+    });
+}, function () {
+    $('#hover').animate({
+        'border-radius': '0'
+    });
+});
+
